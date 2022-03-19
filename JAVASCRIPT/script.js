e31c7d533f93a65ddb98ab6f800ffda8617cc9fd
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-03-20 01:00:21 +0530

    perimetve datatype

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,1 +1,16 @@
 console.log("Hello Github");
+
+
+const pi=3.14;
+//pi=3.12;
+
+console.log(pi);
+
+//DATATYPE
+
+//premitive datatype
+
+let name='Happy Day';
+let age='11';
+console.log(name);
+console.log(age);
